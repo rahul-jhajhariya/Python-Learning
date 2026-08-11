5760494a2296ba2149ace611484e40b189cbd9e1
rahuljhajhariya <jhajhariyaaniket@gmail.com>
2026-08-11 18:04:46 +0530

    home

---
+++ if else practice questions/class.py
@@ -0,0 +1,8 @@
+maths=float(input("Enter your maths marks:"))
+physics=float(input("Enter your physics marks:"))
+chemistry=float(input("Enter your chemistry marks:"))
+computer=float(input("Enter your computer marks:"))
+english=float(input("Enter your english marks:"))
+
+avg=(maths+chemistry+physics+english+computer)/5
+print(avg)
